eb9433e19c9c88cf54b650d121640c444b375c73
brunaporato <brunaporato@gmail.com>
2025-02-01 12:58:54 -0300

    Initial Commit

---
+++ MemorizeAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemorizeAssignment
+//
+//  Created by Bruna Porato on 01/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemorizeAssignment/MemorizeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAssignmentApp.swift
+//  MemorizeAssignment
+//
+//  Created by Bruna Porato on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
