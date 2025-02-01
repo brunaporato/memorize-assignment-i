0ef0df7114326c5863f62134ab6655a75a98c8c3
brunaporato <brunaporato@gmail.com>
2025-02-01 14:35:34 -0300

    add themes

--- MemorizeAssignment/ContentView.swift
+++ MemorizeAssignment/ContentView.swift
@@ -8,26 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🦇", "🎃", "💀", "🕷️", "🧟‍♂️"]
+    let halloweenEmojis = ["👻", "🦇", "🎃", "💀", "🕷️", "🧟‍♂️", "🕸️", "👹"]
+    let pinkEmojis = ["🧞‍♀️", "🎀", "👛", "🌷", "🦩", "🎟️", "💘", "💕"]
+    let carsEmojis = ["🚗", "🚘", "🚙", "🚓", "🚔", "🚕", "🚖", "🏎️"]
+    
+    @State var emojis: [String] = ["👻", "🦇", "🎃", "💀", "🕷️", "🧟‍♂️", "🕸️", "👹"]
+        
+    @State var currentColor = Color.orange
     
     var body: some View {
         VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+                .foregroundColor(currentColor)
+                .fontWeight(.black)
+            
             ScrollView {
                 cards
                 cards
             }
+            
+            themes
         }
         .padding()
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))], spacing: 8) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))], spacing: 8) {
             ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundColor(.orange)
+        .foregroundColor(currentColor)
+    }
+    
+    func themeButton(themeName: String) -> some View {
+        Button(action: {
+            switch themeName {
+            case "Halloween":
+                currentColor = Color.orange
+                emojis = halloweenEmojis
+            case "Pink":
+                currentColor = Color.pink
+                emojis = pinkEmojis
+            case "Cars":
+                currentColor = Color.blue
+                emojis = carsEmojis
+            default:
+                currentColor = Color.orange
+                emojis = halloweenEmojis
+            }
+        },label: {
+            Text(themeName)
+        })
+    }
+    
+    var themes: some View {
+        HStack {
+            themeButton(themeName: "Halloween")
+            Spacer()
+            themeButton(themeName: "Pink")
+            Spacer()
+            themeButton(themeName: "Cars")
+        }
+        .padding()
     }
 }
 
