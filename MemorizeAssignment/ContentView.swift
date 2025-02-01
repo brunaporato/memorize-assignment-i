f23e47baa93fb044e7b8d3dd74aae683095b8905
brunaporato <brunaporato@gmail.com>
2025-02-01 15:07:59 -0300

    add alignment to theme buttons

--- MemorizeAssignment/ContentView.swift
+++ MemorizeAssignment/ContentView.swift
@@ -66,7 +66,7 @@ struct ContentView: View {
     }
     
     var themes: some View {
-        HStack {
+        HStack(alignment: .bottom) {
             themeButton(themeName: "Halloween", symbol: "party.popper.fill")
             Spacer()
             themeButton(themeName: "PinkVibe", symbol: "camera.macro")
